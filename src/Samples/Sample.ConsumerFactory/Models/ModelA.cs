99fc4f9324c70e31bfcf4ae83d3fe17d5c0fd0a6
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-10-03 20:56:56 +0300

    version 4.0.3

--- src/Samples/Sample.ConsumerFactory/Models/ModelA.cs
+++ src/Samples/Sample.ConsumerFactory/Models/ModelA.cs
@@ -3,6 +3,7 @@ using Twino.Client.TMQ.Annotations;
 namespace Sample.ConsumerFactory.Models
 {
     [QueueName("model-a")]
+    [RouterName("model-a")]
     [DirectTarget(FindTargetBy.Name, "receiver-name")]
     public class ModelA
     {
